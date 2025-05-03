25e029b8d58bba2eefb721b41feb9ede05cdc44a
Rina-sketch <nu426957@gmail.com>
2025-05-03 21:48:45 +0300

    Updated server to use Socket.IO

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 const express = require('express');
-const { ExpressPeerServer } = require('peer');
 const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
 const port = process.env.PORT || 9000;
 
 const server = http.createServer(app);
-
-const peerServer = ExpressPeerServer(server, {
-  debug: true,
-  path: '/myapp'
+const io = new Server(server, {
+  cors: {
+    origin: '*', // Разрешить подключения от всех источников (для теста)
+    methods: ['GET', 'POST']
+  }
 });
 
-app.use('/peerjs', peerServer);
-
 app.get('/', (req, res) => {
-  res.send('PeerJS сервер для Неоновой Бродилки запущен!');
+  res.send('Socket.IO сервер для Неоновой Бродилки запущен!');
+});
+
+io.on('connection', (socket) => {
+  console.log('Клиент подключился:', socket.id);
+
+  socket.on('join', (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit('playerJoined', socket.id);
+  });
+
+  socket.on('gameData', (roomId, data) => {
+    socket.to(roomId).emit('gameData', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Клиент отключился:', socket.id);
+  });
 });
 
 server.listen(port, () => {
