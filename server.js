91ac1d6e122f61ac0161887fd65979b894fe2a09
Rina-sketch <nu426957@gmail.com>
2025-05-19 20:24:20 +0300

    Update game.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ io.on('connection', (socket) => {
 
     // Initialize room for host
     rooms[socket.id] = { players: [socket.id] };
-    socket.join(socket.id);
 
     socket.on('joinRoom', (roomId) => {
         if (!rooms[roomId]) {
